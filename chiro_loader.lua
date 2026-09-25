aafd46b8d2a3b7825fedbb4677333ad732d6a0c7
leviiexesc <leviiexesc@github.com>
2026-09-25 13:08:27 +0700

    feat: add chiro_lib.luau standalone UI library (returns Chiro object)

--- chiro_loader.lua
+++ chiro_loader.lua
@@ -1,3 +1,3 @@
 repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer
 getgenv().Key = "CHIRO-XXXX-XXXX-XXXX"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/leviiexesc/chiro_UI/main/chiro_v8.luau"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/leviiexesc/chiro_UI/main/chiro_lib.luau"))()
