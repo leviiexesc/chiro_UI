d2c2d88e09de46e0ddd35558b14aef22aca3f4f8
leviiexesc <leviiexesc@github.com>
2026-09-25 13:39:28 +0700

    fix: safe getgenv() check, fallback to _G.Key

--- chiro_loader.lua
+++ chiro_loader.lua
@@ -1,3 +1,4 @@
 repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer
-getgenv().Key = "CHIRO-XXXX-XXXX-XXXX"
+local _env = (typeof(getgenv) == "function" and getgenv()) or _G
+_env.Key = "CHIRO-XXXX-XXXX-XXXX"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/leviiexesc/chiro_UI/main/chiro_lib.luau"))()
